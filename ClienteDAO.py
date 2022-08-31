b551f5bdd654088ac5ab30ae16d218487bc3b0fe
arthurmichaltchuk <arthurmichaltchuk4@gmail.com>
2022-08-31 20:37:44 -0300

    Cliente OK

--- ClienteDAO.py
+++ ClienteDAO.py
@@ -1,20 +1,27 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
 
+class Cliente(BaseModel):
+    codigo: int = None
+    nome: str
+    cpf: str
+    telefone: str
+    compra_fiado: int = None
+    dia_fiado: int
+    senha: str = None
+
 # Criar os endpoints de Cliente: GET, POST, PUT, DELETE
 @router.get("/cliente/{id}", tags=["cliente"])
-def get_cliente(id: int):
-    return {"msg": "get executado"}, 200
-
+def get_Cliente(id: int):
+    return {"msg": "get executado", "id": id}, 200
 @router.post("/cliente/{id}", tags=["cliente"])
-def post_cliente(id: int):
-    return {"msg": "post executado"}, 200
-
+def post_Cliente(id: int, f: Cliente):
+    return {"msg": "post executado", "id": id, "nome": f.nome, "cpf": f.cpf, "telefone": f.telefone}, 200
 @router.put("/cliente/{id}", tags=["cliente"])
-def put_cliente(id: int):
-    return {"msg": "put executado"}, 201
-
+def put_Cliente(id: int, f: Cliente):
+    return {"msg": "put executado", "id": id, "nome": f.nome, "cpf": f.cpf, "telefone": f.telefone}, 201
 @router.delete("/cliente/{id}", tags=["cliente"])
-def delete_cliente(id: int):
-    return {"msg": "delete executado"}, 201
+def delete_Cliente(id: int):
+    return {"msg": "delete executado", "id": id}, 201
